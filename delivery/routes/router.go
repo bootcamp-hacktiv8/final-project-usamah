f08a324d41c7c013f0c56ba83b5b2901bfff52b3
Usamah Abdurrahman <abdurrahman.usamah07@gmail.com>
2022-06-28 08:42:45 +0700

    update endpoint

--- delivery/routes/router.go
+++ delivery/routes/router.go
@@ -1,40 +1,40 @@
 package routes
 
 import (
-	_commentRouter "final-project-usamah/delivery/routes/comment"
-	_photoRouter "final-project-usamah/delivery/routes/photo"
-	_sosmedRouter "final-project-usamah/delivery/routes/social_media"
-	_userRouter "final-project-usamah/delivery/routes/user"
-	_commentRepository "final-project-usamah/repository/comment"
-	_photoRepository "final-project-usamah/repository/photo"
-	_sosmedRepository "final-project-usamah/repository/social_media"
-	_userRepository "final-project-usamah/repository/user"
+	_commentHandler "final-project-usamah/delivery/handler/comment"
+	_photoHandler "final-project-usamah/delivery/handler/photo"
+	_sosmedHandler "final-project-usamah/delivery/handler/social_media"
+	_userHandler "final-project-usamah/delivery/handler/user"
+	"final-project-usamah/delivery/middlewares"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
 
-func Routes(
-	userRepository _userRepository.UserRepositoryInterface,
-	photoRepository _photoRepository.PhotoRepositoryInterface,
-	commentRepository _commentRepository.CommentRepositoryInterface,
-	sosmedRepository _sosmedRepository.SosmedRepositoryInterface,
-) *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	mount(router, "/users", _userRouter.UserResource{}.UserRoute(userRepository))
-	mount(router, "/photos", _photoRouter.PhotoResource{}.PhotoRoute(photoRepository))
-	mount(router, "/comments", _commentRouter.CommentResource{}.CommentRoute(commentRepository))
-	mount(router, "/socialmedias", _sosmedRouter.SosmedResource{}.SosmedRoute(sosmedRepository))
-
-	return router
+func UserAuthPath(r *mux.Router, uh *_userHandler.UserHandler) {
+	r.HandleFunc("/users/register", uh.RegisterHandler).Methods("POST")
+	r.HandleFunc("/users/login", uh.LoginHandler).Methods("POST")
+	r.Handle("/users", middlewares.Authentication(http.HandlerFunc(uh.UpdateUserHandler))).Methods("PUT")
+	r.Handle("/users", middlewares.Authentication(http.HandlerFunc(uh.DeleteUserHandler))).Methods("DELETE")
 }
 
-func mount(r *mux.Router, path string, handler http.Handler) {
-	r.PathPrefix(path).Handler(
-		http.StripPrefix(
-			strings.TrimSuffix(path, "/"),
-			handler,
-		),
-	)
+func PhotoPath(r *mux.Router, ph *_photoHandler.PhotoHandler) {
+	r.Handle("/photos", middlewares.Authentication(http.HandlerFunc(ph.CreatePhotoHandler))).Methods("POST")
+	r.Handle("/photos", middlewares.Authentication(http.HandlerFunc(ph.GetAllPhotoHandler))).Methods("GET")
+	r.Handle("/photos/{photoId}", middlewares.Authentication(http.HandlerFunc(ph.UpdatePhotoHandler))).Methods("PUT")
+	r.Handle("/photos/{photoId}", middlewares.Authentication(http.HandlerFunc(ph.DeletePhotoHandler))).Methods("DELETE")
+}
+
+func CommentPath(r *mux.Router, ch *_commentHandler.CommentHandler) {
+	r.Handle("/comments", middlewares.Authentication(http.HandlerFunc(ch.CreateCommentHandler))).Methods("POST")
+	r.Handle("/comments", middlewares.Authentication(http.HandlerFunc(ch.GetAllCommentHandler))).Methods("GET")
+	r.Handle("/comments/{commentId}", middlewares.Authentication(http.HandlerFunc(ch.UpdateCommentHandler))).Methods("PUT")
+	r.Handle("/comments/{commentId}", middlewares.Authentication(http.HandlerFunc(ch.DeleteCommentHandler))).Methods("DELETE")
+}
+
+func SocialMediaPath(r *mux.Router, sh *_sosmedHandler.SosmedHandler) {
+	r.Handle("/socialmedias", middlewares.Authentication(http.HandlerFunc(sh.CreateSosmedHandler))).Methods("POST")
+	r.Handle("/socialmedias", middlewares.Authentication(http.HandlerFunc(sh.GetAllSosmedHandler))).Methods("GET")
+	r.Handle("/socialmedias/{socialMediaId}", middlewares.Authentication(http.HandlerFunc(sh.UpdateSosmedHandler))).Methods("PUT")
+	r.Handle("/socialmedias/{socialMediaId}", middlewares.Authentication(http.HandlerFunc(sh.DeleteSosmedHandler))).Methods("DELETE")
 }
--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	_configs "final-project-usamah/configs"
-	_routes "final-project-usamah/delivery/routes"
-	_commentRepository "final-project-usamah/repository/comment"
-	_photoRepository "final-project-usamah/repository/photo"
-	_sosmedRepository "final-project-usamah/repository/social_media"
+	"final-project-usamah/delivery/middlewares"
+	"final-project-usamah/delivery/routes"
+
+	_userHandler "final-project-usamah/delivery/handler/user"
 	_userRepository "final-project-usamah/repository/user"
+	_userService "final-project-usamah/service/user"
+
+	_photoHandler "final-project-usamah/delivery/handler/photo"
+	_photoRepository "final-project-usamah/repository/photo"
+	_photoService "final-project-usamah/service/photo"
+
+	_commentHandler "final-project-usamah/delivery/handler/comment"
+	_commentRepository "final-project-usamah/repository/comment"
+	_commentService "final-project-usamah/service/comment"
+
+	_sosmedHandler "final-project-usamah/delivery/handler/social_media"
+	_sosmedRepository "final-project-usamah/repository/social_media"
+	_sosmedService "final-project-usamah/service/social_media"
+
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -21,19 +36,31 @@ func main() {
 	fmt.Println("Successfully connected to database")
 
 	userRepository := _userRepository.NewUserRepository(db)
-	photoRepository := _photoRepository.NewPhotoRepository(db)
-	commentRepository := _commentRepository.NewCommentRepository(db)
-	sosmedRepository := _sosmedRepository.NewSosmedRepository(db)
+	userService := _userService.NewUserService(userRepository)
+	userHandler := _userHandler.NewUserHandler(userService)
 
-	router := _routes.Routes(
-		userRepository,
-		photoRepository,
-		commentRepository,
-		sosmedRepository,
-	)
+	photoRepository := _photoRepository.NewPhotoRepository(db)
+	photoService := _photoService.NewPhotoService(photoRepository)
+	photoHandler := _photoHandler.NewPhotoHandler(photoService)
+
+	commentRepository := _commentRepository.NewCommentRepository(db)
+	commentService := _commentService.NewCommentService(commentRepository)
+	commentHandler := _commentHandler.NewCommentHandler(commentService)
+
+	sosmedRepository := _sosmedRepository.NewSosmedRepository(db)
+	sosmedService := _sosmedService.NewSosmedService(sosmedRepository)
+	sosmedHandler := _sosmedHandler.NewSosmedHandler(sosmedService)
+
+	r := mux.NewRouter()
+	r.Use(middlewares.LoggingMiddleware)
+
+	routes.UserAuthPath(r, userHandler)
+	routes.PhotoPath(r, photoHandler)
+	routes.CommentPath(r, commentHandler)
+	routes.SocialMediaPath(r, sosmedHandler)
 
 	srv := &http.Server{
-		Handler: router,
+		Handler: r,
 		Addr:    "0.0.0.0:8080",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
